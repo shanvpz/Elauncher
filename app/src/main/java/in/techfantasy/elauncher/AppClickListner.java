946aa44d2deb0787e2ae8de5d74d7d3ca4ce3b28
jobinjnj <9847116011.zion>
2017-12-29 15:27:17 +0530

    App click listner

---
+++ app/src/main/java/in/techfantasy/elauncher/AppClickListner.java
@@ -0,0 +1,17 @@
+package in.techfantasy.elauncher;
+
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.view.View;
+
+/**
+ * Created by campusiq on 29/12/17.
+ */
+
+public class AppClickListner implements View.OnClickListener {
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
