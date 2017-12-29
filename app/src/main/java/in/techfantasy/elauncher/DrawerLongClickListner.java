68401d32d18048ff29c5978aad45e3bc70680df1
jobinjnj <9847116011.zion>
2017-12-29 15:14:23 +0530

    long click listner work 2

--- app/src/main/java/in/techfantasy/elauncher/DrawerLongClickListner.java
+++ app/src/main/java/in/techfantasy/elauncher/DrawerLongClickListner.java
@@ -1,10 +1,14 @@
 package in.techfantasy.elauncher;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SlidingDrawer;
+import android.widget.TextView;
 
 /**
  * Created by campusiq on 29/12/17.
@@ -23,6 +27,19 @@ public class DrawerLongClickListner implements AdapterView.OnItemLongClickListen
 
     @Override
     public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        MainActivity.appLaunchable=false;
+        RelativeLayout.LayoutParams lp=new RelativeLayout.LayoutParams(view.getWidth(),view.getHeight());
+        lp.leftMargin= (int) view.getX();
+        lp.topMargin= (int) view.getY();
+        LayoutInflater li=(LayoutInflater)ctx.getSystemService(ctx.LAYOUT_INFLATER_SERVICE);
+        LinearLayout ll= (LinearLayout) li.inflate(R.layout.draweritem,null);
+        ((ImageView)ll.findViewById(R.id.icon_image)).setImageDrawable(((ImageView)view.findViewById(R.id.icon_image)).getDrawable());
+        ((TextView)ll.findViewById(R.id.icon_text)).setText(((TextView)view.findViewById(R.id.icon_text)).getText());
+
+
+        homeview.addView(ll,lp);
+        drawer.animateClose();
+        drawer.bringToFront();
         return false;
     }
 }
