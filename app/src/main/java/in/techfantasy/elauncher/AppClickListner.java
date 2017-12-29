96683aebcf8a712eec1cec44ceefeee0a8403bbc
jobinjnj <9847116011.zion>
2017-12-29 15:42:25 +0530

    App click listner 02

--- app/src/main/java/in/techfantasy/elauncher/AppClickListner.java
+++ app/src/main/java/in/techfantasy/elauncher/AppClickListner.java
@@ -1,7 +1,9 @@
 package in.techfantasy.elauncher;
 
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 
 /**
@@ -9,9 +11,22 @@ import android.view.View;
  */
 
 public class AppClickListner implements View.OnClickListener {
+    Context ctx;
+    Item[] packs;
+
+    public AppClickListner(Context ctx, Item[] packs) {
+        this.ctx = ctx;
+        this.packs = packs;
+    }
 
     @Override
     public void onClick(View view) {
-
+        String [] data;
+        data= (String[]) view.getTag();
+        Intent launch = new Intent(Intent.ACTION_MAIN);
+        launch.addCategory(Intent.CATEGORY_LAUNCHER);
+        ComponentName cn=new ComponentName(data[0],data[1]);
+        launch.setComponent(cn);
+        ctx.startActivity(launch);
     }
 }
