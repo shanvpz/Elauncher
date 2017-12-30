0121d740e94f391e2f3a820be355a63a47c3c9e6
jobinjnj <9847116011.zion>
2017-12-30 14:55:34 +0530

    java file items updated

--- app/src/main/java/in/techfantasy/elauncher/Item.java
+++ app/src/main/java/in/techfantasy/elauncher/Item.java
@@ -1,8 +1,14 @@
 package in.techfantasy.elauncher;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.util.Config;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 import static in.techfantasy.elauncher.MainActivity.*;
@@ -18,13 +24,50 @@ public class Item implements Serializable{
     String packageName;
     boolean visible;
     int x,y;
-    String itemlocation;
+    String iconlocation;
+    boolean landscape;
     public void cacheIcon(){
-        if(itemlocation==null)
-          new File(MainActivity.ac.getApplicationInfo().dataDir+"/cachedApps/").mkdirs();
+        if(iconlocation==null)
+          new File(MainActivity.activity.getApplicationInfo().dataDir+"/cachedApps/").mkdirs();
+        if(icon!=null){
+            iconlocation=MainActivity.activity.getApplicationInfo().dataDir+"/cachedApps/"+packageName+name;
+            FileOutputStream fos=null;
+            try{
+                fos = new FileOutputStream(iconlocation);
+            }catch(FileNotFoundException e){
+                e.printStackTrace();
+            }
+            if (fos!=null){
+                Tools.drawableToBitmap(icon).compress(Bitmap.CompressFormat.PNG,100,fos);
+                try{
+                    fos.flush();
+                    fos.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }else{
+                iconlocation=null;
+            }
+        }
 
     }
-    public void deleteIcon(){
 
+    public Bitmap getCachedIcon(){
+        BitmapFactory.Options options=new BitmapFactory.Options();
+        options.inJustDecodeBounds = false;
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        options.inDither=true;
+
+        if(iconlocation!=null){
+            File cachedIcon=new File(iconlocation);
+            if(cachedIcon.exists()){
+                return BitmapFactory.decodeFile(cachedIcon.getAbsolutePath(),options);
+            }
+        }
+        return null;
+    }
+    public void deleteIcon(){
+        if(iconlocation!=null)
+             new File(iconlocation).delete();
     }
 }
